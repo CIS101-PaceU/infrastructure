559a6488a2ee3a009feb1ff59c52c7fdce99b534
VaishnaviSowany <vaishnavisowany@gmail.com>
2017-10-10 15:32:28 -0400

    Update html_grading.php

--- app/student/html/html_grading.php
+++ app/student/html/html_grading.php
@@ -1,1 +1,19 @@
-<p>This is the html grading page. Put your content here.</p>
+<!DOCTYPE html>
+<html>
+<style>
+div {
+   width: 60%;
+   
+}
+</style>
+<body>
+<div>
+<form action="uploads.php" method="post" enctype="multipart/form-data">
+    Select html page to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+<br/>
+    <input type="submit" value="Upload File" name="submit">
+</form>
+<div>
+</body>
+</html>
