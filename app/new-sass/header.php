499938f60342e7c673ca869bb74bd6b35b045f08
vanessa rene <vjrene@gmail.com>
2017-10-24 19:43:56 -0400

    New sass (#48)
    * New sass for redesign-- new log in page, some navigation
    
    * Update sidebar navigation, some Instructor-Announcements
    
    * Update code and styles for login page

---
+++ app/new-sass/header.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+    <div class="side-nav--bio">
+      <h3>Welcome, Professor!</h3>
+      <p class="side-nav--bio--userid"><i class="fa fa-user" aria-hidden="true"></i> U9021087</p>
+      <p class="side-nav--bio--username"><i class="fa fa-envelope-o" aria-hidden="true"></i> prof@pace.edu</p>
+      <p class="side-nav--bio__section"><i class="fa fa-star" aria-hidden="true"></i> W | 03:30 PM - 05:30PM</p>
+    </div>
+
+    <div class="side-nav--links">
+      <h2>NAVIGATION</h2>
+
+      <div class="side-nav--nav__section">
+
+        <form id="chooseclass-form" action ="" method = "post">
+          <div class="styled-select">
+          <select name="chooseclass-select" class="chooseclass-select_class">
+
+        <optgroup>
+          <option>W | 03:30 PM - 05:30PM</option>
+          <option>T | 12:30 PM - 2:30PM</option>
+
+        </optgroup>
+      </select>
+    </div>
+    </form>
+
+      </div>
+
+      <ul>
+         <li><a class="nav-button" href="#"><i class="fa fa-home" aria-hidden="true"></i> DASHBOARD</a></li>
+         <li><a class="nav-button" href="#"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> ASSIGNMENTS</a></li>
+         <li><a class="nav-button" href="#"><i class="fa fa-quote-left" aria-hidden="true"></i> DISCUSSION BOARD</a></li>
+      </ul>
+
+      <ul>
+        <li><a class="nav-button" href="#"><i class="fa fa-cog" aria-hidden="true"></i> SETTINGS</a></li>
+        <li><a class="nav-button" href="#"><i class="fa fa-sign-out" aria-hidden="true"></i> sign out</a></li>
+
+      </ul>
+    </div>
+
+
+   
+
+</body>
+</html>
+  
