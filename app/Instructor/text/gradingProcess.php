d0bc2b45d1959f2447c3b491d96b3653210bf966
Nishant Koli <nishantkoli@Nishants-MacBook-Air.local>
2017-11-26 19:37:35 -0500

    Grade can be assigned and updated by the instructor

---
+++ app/Instructor/text/gradingProcess.php
@@ -0,0 +1,57 @@
+<?php
+
+$belowRoot = true;
+$isLoggedIn = true;
+$showNav = true;
+$displayClass =true;
+$isTeacher = true;
+
+$thisPage="Post Text Grade";
+
+include('../../header.php');
+include('../../config.php');
+
+
+/*
+
+echo $_POST["assName"],$_POST["Instructions"],$_POST["dueDate"];
+
+$sql="INSERT into text_assignment (assName,Instructions,dueDate)
+VALUES
+('".$_POST["assName"]."','".$_POST["Instructions"]."','".$_POST["dueDate"]."')";
+
+echo $getGrade;
+echo $getSubId;
+echo $getAssId;
+echo $getStudId;
+
+Instructor/text/textSubmissions.php?assId=1
+
+
+*/
+
+$getGrade = $_GET["grade"];
+$getSubId = $_GET["subId"];
+$getAssId = $_GET["assId"];
+$getStudId = $_GET["studId"];
+
+$sql="INSERT into text_grades (studId,assId,grade,subId)
+VALUES
+('".$getStudId."','".$getAssId."','".$getGrade."','".$getSubId."')";
+
+if($conn->query($sql) === TRUE)
+{
+    echo "Your post was submitted. Go <a href='textSubmissions.php?assId=".$getAssId."'>back</a> or go <a href='classHome.php'>home</a>.";
+} else {
+    echo "An error. Contact the adminstrator. Or go <a href='classHome.php'>home</a>.";
+}
+
+$conn->close();
+
+
+//echo $_POST["studId"],$_POST["assId"],$_POST["grade"],$_POST["subId"];
+
+include('../../footer.php');
+
+
+?>
--- app/Instructor/text/textSubmissions.php
+++ app/Instructor/text/textSubmissions.php
@@ -16,26 +16,6 @@ include('../../header.php');
 include('../../config.php');
 ?>
 
-    <!-- GENERATE DIVS BASED ON SUBMISSION TABLE 
-    <div class="prev-update">
-    <h2>Student Name</h2>
-        <p>Student notes if they have added them.</p>
-        <p><a href="">AssignmentFile.xlxs</a></p>
-        
-        <div class="auto-grade">
-            automated grades pulled from grading tool
-        </div>  
-    </div>
-    
-        <div class="prev-update">
-    <h2>Student Name</h2>
-        <p>Student notes if they have added them.</p>
-        <p><a href="">AssignmentFile.xlxs</a></p>
-        
-        <div class="auto-grade">
-            automated grades pulled from grading tool
-        </div>  
-    </div> -->
     <div class="main-container">
     <center>
     <div class="all-updates">
@@ -48,13 +28,16 @@ include('../../config.php');
     //$conn = new mysqli("localhost","root","", "Red-Velvet");
     $assignId = $_GET["assId"];
                         if(!$conn) {echo "error";}                    
-    $latestPostSQL ="SELECT * from text_submission where assId=".$assignId;
-                    
+    $latestPostSQL ="SELECT t1.assId,t1.studId,t1.subDate,t2.grade,t1.assText,t1.subId from text_submission t1 LEFT JOIN text_grades t2 on t1.subId=t2.subId where t1.assId=".$assignId;
+    //SELECT * from text_submission t1 LEFT JOIN text_grades t2 on t1.subId=t2.subId where t1.assId=1      
+    //SELECT * from text_submission where assId=".$assignId  
     $result = $conn->query($latestPostSQL);
     if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-    echo "<div class='prev-update'><h2>" . $row["studId"] . "</h2>" . "<span class='bold-text'>Submission Date: " . $row["subDate"] . "</span><br>Text Submission: " . $row["assText"] . "<br><a href='textSubmissions.php'>Grade</a><form action='/action_page.php'>
+    echo "<div class='prev-update'><h2>" . $row["studId"] . "</h2>" . "<span class='bold-text'>Submission Date: " . $row["subDate"] . "</span><br>Text Submission: " . $row["assText"];
+    if($row["grade"]==null){
+    echo "<br><a href='textSubmissions.php'>Grade</a><form action='gradingProcess.php'>
     <input list='grades' name='grade'>
     <datalist id='grades'>
       <option value='A+'>
@@ -71,8 +54,35 @@ include('../../config.php');
       <option value='D-'>
       <option value='F'>
     </datalist>
-    <input type='submit'>
-  </form></div>";
+    <input type='text' name='subId' value='" . $row['subId'] ."' style='display: none;'>
+    <input type='text' name='studId' value='" . $row['studId'] ."' style='display: none;'>
+    <input type='text' name='assId' value='" . $assignId ."' style='display: none;'>
+    <button type='submit'>Add</button>
+  </form>";} else {
+      echo "<br><a href='textSubmissionsEdit.php'>Grade</a><form action='gradingEditProcess.php'>
+      <input list='grades' name='grade' value='". $row['grade'] ."'>
+      <datalist id='grades'>
+        <option value='A+'>
+        <option value='A'>
+        <option value='A-'>   
+        <option value='B+'>
+        <option value='B'>
+        <option value='B-'>
+        <option value='C+'>
+        <option value='C'>
+        <option value='C-'>
+        <option value='D+'>
+        <option value='D'>
+        <option value='D-'>
+        <option value='F'>
+      </datalist>
+      <input type='text' name='subId' value='" . $row['subId'] ."' style='display: none;'>
+      <input type='text' name='studId' value='" . $row['studId'] ."' style='display: none;'>
+      <input type='text' name='assId' value='" . $assignId ."' style='display: none;'>
+      <button type='submit'>Edit</button>
+    </form>";
+  }
+  echo "</div>";
         }
     } else {
     echo "";
