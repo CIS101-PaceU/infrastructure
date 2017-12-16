b952c313e5c04387ddbbddf867aaa10ebb4aaa50
Ricky Jairath <rckjairath@gmail.com>
2017-12-15 22:09:56 -0500

    added excel team files (#53)
    * added excel team files
    
    * Updates Excel Files
    
    * Final Update with New UI

--- app/student/excel/index.php
+++ app/student/excel/index.php
@@ -13,12 +13,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-
-  <?php 
-  include('../../header.php');
-  include('../../mobile-nav.php');
+<?php
+include ('../../header.php');
+include('../../mobile-nav.php');
 ?>
-
 
 <div class="main-page">
   
