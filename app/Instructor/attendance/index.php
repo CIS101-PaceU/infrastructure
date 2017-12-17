eb052271fcf95b524a7de3f8142cb12074d7a87a
daxpatel1 <dp00174n@pace.edu>
2017-12-17 11:29:14 -0500

    Added all the files

---
+++ app/Instructor/attendance/index.php
@@ -0,0 +1,39 @@
+<?php 
+
+ $activePage = 'attendance';
+ $isInstructor = true;
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Instructor -- Attendance</title>
+    <link rel="stylesheet" type="text/css" href="../../stylesheets/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+
+    <?php 
+    include('../../header.php');
+    include('../../mobile-nav.php');
+  ?>
+
+
+  <div class="main-page">
+    
+      <?php 
+        include("../../navigation.php");
+        ?>
+      
+
+    <div class="main-content">
+      <?php 
+      include('attendance.php');
+    ?>
+
+    </div>
+
+  </div>
+  </body>
+</html>
