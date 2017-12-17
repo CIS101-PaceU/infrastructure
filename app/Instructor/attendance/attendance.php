1c4b812b5ed9a4b95ba5c47196ece195b67d69f0
daxpatel1 <dp00174n@pace.edu>
2017-12-17 11:14:30 -0500

    Commit

--- app/Instructor/attendance/attendance.php
+++ app/Instructor/attendance/attendance.php
@@ -1,37 +1,63 @@
 <!DOCTYPE html>
 <html>
-	<?php include 'Config.php'; ?>
+
+<?php
+  $servername = "phpmyadmin.cw74xrxhh0s2.us-east-1.rds.amazonaws.com";
+  $username = "phpmyadmin";
+  $password = "CIS101portal-db";
+  $dbname = "portal-db";
+  $port = 3306;
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+  // Check connection
+  if ($conn->connect_error) 
+  {
+    die("Connection failed: " . $conn->connect_error);
+  } 
+
+  $query = "SELECT * FROM events";
+  mysqli_query($conn, $query) or die(' error querrring the datbase');
+
+  $result = mysqli_query($conn, $query);
+  $row = mysqli_fetch_array($result);
+?>
+
 
 <head>
-	<title>Attendance</title>
-	<meta charset="utf-8">
-  	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-	<link rel="stylesheet" type="text/css" href="/stylesheets/main.css">
-  	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <title>Attendance</title>
+  <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+  
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    
     <script>
       <!--date picker-->
- 	    $( function() 
- 	    {
-    	 $( "#datepicker" ).datepicker({ minDate: 0 });
-  	  });
- 		
+      $( function() 
+      {
+       $( "#datepicker" ).datepicker({ minDate: 0 });
+      });
+    
       <!--slider-->
-  	   $( function() 
-  	   {
-    	   $( "#slider" ).slider(
-    	   {
-      	   range: "min",
-      	   min: 1,
-      	   max: 20,
-      	   value: 10,
-    		    slide: function( event, ui ) 
+       $( function() 
+       {
+         $( "#slider" ).slider(
+         {
+           range: "min",
+           min: 1,
+           max: 20,
+           value: 10,
+            slide: function( event, ui ) 
             {
-        	     $( "#amount" ).val( ui.value );
-      	     }
-    	   });
+               $( "#amount" ).val( ui.value );
+             }
+         });
           $( "#amount" ).val($( "#slider" ).slider( "value" ));
-  	   });
+       });
 
        <!--validate form-->
        function validateForm() 
@@ -50,43 +76,35 @@
 </head>
 
 <body>
-	<div id="contents"style="padding: 70px; border: 1px solid #000000; margin: 25px; ">
-		<h1>ATTENDANCE</h1>
-		
-		<form name="Attendance" style="align-content: center;" action='qrpage.php' method='post' onsubmit="return validateForm()">
-			<table style="align-content: centre;" >
-				<TR>
-					<TD> <label>Date: </label></TD>
-					<TD><input type='text' style="height: 20px; width: 200px;" name='date' id='datepicker' readonly="readonly" required></td>
-				</TR>
-				
-				<TR>
-					<TD><label>Event Name: </label></TD>
-          			<TD>
-						<select type='text' name='text' id='text' required style="height: 25px; width: 205px;">
-          			  	<?php
-             				 do
-              				{
-           			 	?>
-								<option value="<?php echo $row['Event_id'];?>"><?php echo $row['Event_name']; ?></option>";
-						 <?php
-						 	}while($row = mysqli_fetch_array($result))
-						 ?>
-						 </select>
-					</TD> 
-				</TR>
-				 <TR>
-				 	<TD><label>Active Time: </label> <div id='slider' style="width:160px;"></div></TD>
-					<TD><input name='time' id='amount' maxlength="2" size="2" readonly="readonly" required style="height: 20px; width: 200px;"><label>min</label></TD>
-				</TR>
-				<br>
-				<TR>
-					
-					<TD> <BR> <input type='submit' value='Submit' readonly="readonly"></TD>
-				</TR>
-			</TABLE>
+  <div id="contents">
+    <h1>Attendance</h1>
+      <form name="Attendance" style="text-align: center;" action='qrpage.php' method='post' onsubmit="return validateForm()"><br>
+        <label>Date:  <br><input type='text' name='date' id='datepicker' readonly="readonly" required><br></label>
+        <label>Event Name: <br>
+          <select type='text' name='text' id='text' required>
+            <?php
+              do
+              {
+            ?>
+                <option value="<?php echo $row['Event_id'];?>"><?php echo $row['Event_name']; ?></option>";
+            <?php
+              }while($row = mysqli_fetch_array($result))
+            ?>
+            ?>
+          </select>
+          <br></label>
+        <label>Active Time:<br>
+            <input name='time' id='amount' maxlength="2" size="2" readonly="readonly" required><label>Minutes</label>
+          <div id='slider' style="width:160px; text-align: center;""></div>
+        </label>
+        <br>
+        <input type='submit' value='Submit' readonly="readonly">
       </form>
-	 </div>
+  </div>
 </body>
 
 </html>
+
+
+
+
--- app/Instructor/attendance/qrpage.php
+++ app/Instructor/attendance/qrpage.php
@@ -1,9 +1,4 @@
-<?php 
-
- $activePage = 'attendance';
- $isInstructor = true;
-
-?>
+<!DOCTYPE html>
 
 <?php include 'Config.php'; ?>
 
@@ -12,8 +7,6 @@
 <head>
 	<title>Generated QR code</title>
 	<meta charset="UTF-8">
-	 <link rel="stylesheet" type="text/css" href="../../stylesheets/main.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
 	<script type="text/javascript">
 		var seconds = <?php echo $_POST['time'] ?>*60; //converting min to sec
@@ -26,11 +19,11 @@
               		remainingSeconds = "0" + remainingSeconds;
               		time =1;
           		}
-          		document.getElementById('countdown').innerHTML =  minutes + ":" + remainingSeconds;
+          		document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
           		if (seconds == 0) 
           		{
               		document.getElementById('countdown').innerHTML = "Times Up!!";
-              		document.getElementById('image').innerHTML = "<img src=\'Late_0.png\' height='550' width='550' >" ;
+              		document.getElementById('image').innerHTML = "<br> <br> <img src=\'Late_0.png\' height='500' width='500' align='center'>" ;
               	} 
               	else 
               	{
@@ -44,57 +37,44 @@
 <body>
 
 
-    <?php 
-    include('../../header.php');
-    include('../../mobile-nav.php');
-  ?>
-
-
-  <div class="main-page">
-    
-      <?php 
-        include("../../navigation.php");
-        ?>
-      
-
-    <div class="main-content">
-			<div id="contents_qrpage" style="padding: 70px; border: 1px solid #000000; margin: 25px;">
-					<div id="timer" style="text-align: center;">
-						<h1 style="font-size: 50px; text-align: center;"><time id="countdown"><?php echo $_POST['time']?>:00</time></h1>
-					</div>
-					<div id="image" style="text-align: center;">
-						<?php
-							$input_text = $_POST['text'];
-							$input_date = $_POST['date'];
-							$minutes = $_POST['time'];
-							$seconds = time() + $minutes*60;
+	<div id="contents_qrpage">
+		<div id="timer">
+			<h1><time id="countdown"><?php echo $_POST['time']?>:00</time></h1>
+		</div>
+		<div id="image">
+			<?php
 				
-							date_default_timezone_set("America/New_York");
-							$startTime = date("Y-m-d H:i:s");
-							$endTime = date('Y-m-d H:i:s', $seconds);
+				$input_text = $_POST['text'];
+				$input_date = $_POST['date'];
+				$minutes = $_POST['time'];
+				$seconds = time() + $minutes*60;
 				
-							echo "<img src='qr_img.php?d=$input_text' height='550' width='550' >";
+				date_default_timezone_set("America/New_York");
+				$startTime = date("Y-m-d H:i:s");
+				$endTime = date('Y-m-d H:i:s', $seconds);
+				
+				echo "<img src='qr_img.php?d=$input_text' height='500' width='500' align='center'>";
           		
-          					$sql_id = mysqli_real_escape_string($conn,$_POST["text"]);
-          					$sql_time_start = mysqli_real_escape_string($conn, $start_time);
-          					$sql_active_time = mysqli_real_escape_string($conn, $minutes);
+          		$sql_id = mysqli_real_escape_string($conn,$_POST["text"]);
+          		$sql_time_start = mysqli_real_escape_string($conn, $start_time);
+          		$sql_active_time = mysqli_real_escape_string($conn, $minutes);
           		
-          					$query_y = " UPDATE events 
-          						SET Active = 'Y', Active_Time = $sql_active_time, Start_Time = '$startTime', End_Time = '$endTime'
-          					 	WHERE Event_id = $sql_id ";
-          					mysqli_query($conn, $query_y) or die(' error 1 querrring the datbase');
+          		$query_y = " UPDATE events 
+          					 SET Active = 'Y', Active_Time = $sql_active_time, Start_Time = '$startTime', End_Time = '$endTime'
+          					 WHERE Event_id = $sql_id ";
+          		mysqli_query($conn, $query_y) or die(' error 1 querrring the datbase');
           		
-          					do
-          					{
-            							$query_n = " UPDATE events 
-            					 		SET Active = 'N', Active_Time = 0, Start_Time = 0, End_Time = 0
-            					 		WHERE Event_id <> $sql_id ";
-            						mysqli_query($conn, $query_n) or die(' error 2 querrring the datbase');
-          					}while($row = mysqli_fetch_array($result))
-						?>
-					</div>
-				</div>
-	    		</div>
+          		do
+          		{
+            		$query_n = " UPDATE events 
+            					 SET Active = 'N', Active_Time = 0, Start_Time = 0, End_Time = 0
+            					 WHERE Event_id <> $sql_id ";
+            		
+            		mysqli_query($conn, $query_n) or die(' error 2 querrring the datbase');
+          		}while($row = mysqli_fetch_array($result))
+			?>
+		</div>
+	</div>
 </body>
 </html>
 
